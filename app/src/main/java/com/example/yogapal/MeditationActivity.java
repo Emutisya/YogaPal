f5548a89600a6bdace52c0334d06e007fbf20c2c
Emutisya <mutisya.elizabeth@strathmore.edu>
2020-07-09 03:59:28 +0300

    Updated UI

--- app/src/main/java/com/example/yogapal/MeditationActivity.java
+++ app/src/main/java/com/example/yogapal/MeditationActivity.java
@@ -2,13 +2,41 @@ package com.example.yogapal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MeditationActivity extends AppCompatActivity {
-
+    Button btnTimer, btnPlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meditation);
+
+        btnTimer =(Button)findViewById(R.id.btnTimer);
+        btnTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MeditationActivity.this,timer.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+
+        btnPlay =(Button)findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MeditationActivity.this,MediationMusicActivity.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+
     }
 }
